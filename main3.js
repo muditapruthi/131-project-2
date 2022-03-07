2b5becaab6d5430a50d3bc13f993b1b38916c0f6
muditapruthi <98452400+muditapruthi@users.noreply.github.com>
2022-03-07 22:22:53 +0530

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,48 @@
+objectDetector="";
+
+img="";
+objects=[];
+status="";
+
+function preload(){
+    img=loadImage('desk.jpg')
+}
+
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd,modelLoaded');
+    document.getElementById("status").innerHTML="status:Detecting Objects";
+}
+
+function modelLoaded () {
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(erorr, results) {
+    if (erorr) {
+        console.log(erorr);
+    }
+    console.log(results);
+    object=results;
+}
+
+function draw() {
+    image( img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+        for(var i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+
+            Fill(255, 0, 0);
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15 , objects[i].y+15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+        }
+    }
+}
